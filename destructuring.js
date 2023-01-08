4b6426aad8e7ef5ac7ba8d384aa0e205a6ebf817
Harold Barleta <hbf20c@gmail.com>
2023-01-08 00:58:36 -0800

    destructuring assignment completed

---
+++ destructuring.js
@@ -0,0 +1,59 @@
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+//'Tesla' and 'Mercedes' are logged in console
+
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+// this will create an error, 'name' is not defined since it was renamed to other Name when destructured 
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+console.log(password);
+// '12345' will log in console however, password does not exist in person object so hashedPassword will return undefined when console.log is called
+
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+// false and true will be logged
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+// 'value' is logged, secondKey array is logged, 1 is logged since it is the first index of the array, 5 is logged since it is the second element of secondKey variable
+
+
