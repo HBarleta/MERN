ac986738d0f749a91cbf4345684c5fe83e015266
Harold Barleta <hbf20c@gmail.com>
2023-01-12 09:11:43 -0800

    added algos

---
+++ javascript/algos/week1day4.js
@@ -0,0 +1,92 @@
+/* 
+    Stable sort.
+    Visualization:
+    https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/visualize/
+    Time Complexity
+        - Best case: O(n log(n)) linearithmic.
+        - Average case: O(n log(n)) linearithmic.
+        - Worst case: O(n log(n)) linearithmic.
+    Space: O(n) linear
+    steps:
+        1. create a merge function to merge two already sorted arrays into a single
+            sorted array.
+        - you do NOT need to work in place, ok to use a new array
+
+        2. create mergeSort function to sort the provided array
+        - split the array in half and recursively merge the halves using the
+            previously created merge function.
+        - splitting of arrays stops when array can no longer be split.
+        - an array of 1 item is by definition sorted, so two arrays of 1 item
+            can therefore be merged into a sorted array.
+*/
+
+// merge
+
+
+/**
+ * Efficiently merges two already sorted arrays into a new sorted array.
+ * Do not mutate the given arrays.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} left
+ * @param {Array<number>} right
+ * @returns {Array<number>} A new sorted array containing all the elements of
+ *    both given halves.
+ * 
+ */
+const sortedA1 = [];
+const sortedB1 = [];
+const expectedMerge1 = [];
+
+const sortedA2 = [5];
+const sortedB2 = [2];
+const expectedMerge2 = [2, 5];
+
+const sortedA3 = [3];
+const sortedB3 = [2, 3, 4, 7];
+const expectedMerge3 = [2, 3, 3, 4, 7];
+
+function merge(left, right) {
+    let results = [];
+    let i = 0;
+    let j = 0;
+    while (i < left.length || j < right.length){
+        if(left[i] < right[j]){
+            results.push(left[i]);
+            i++;
+            console.log(" this is left " + left[i])
+        } else if (left[i] > right[j]) {
+            results.push(right[j])
+            console.log("this is right " + right[j])
+            j++;
+        } else {
+            results.push(left[i], right[j])
+            i++;
+            j++;
+        }
+    }
+    if (i > left.length){
+    
+    }
+    console.log()
+    return results
+}
+console.log(merge(sortedA3, sortedB3));
+
+/**
+ * Creates a new sorted array based on the given nums being recursively split
+ * and merged.
+ * Best: O(n log(n)) linearithmic.
+ * Avg: O(n log(n)) linearithmic.
+ * Worst: O(n log(n)) linearithmic.
+ * @param {Array<number>} nums
+ * @returns {Array<number>} A New sorted array.
+ */
+function mergeSort(nums) {
+
+}
+
+/*****************************************************************************/
+
+
+// split arrays into half until only 2 elements remain
