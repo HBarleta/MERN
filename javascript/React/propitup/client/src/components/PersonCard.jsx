cb812c655a4d13002dced93a24dbe7853a518027
Harold Barleta <hbf20c@gmail.com>
2023-01-13 05:26:21 -0800

    added birthday Button

--- javascript/React/propitup/client/src/components/PersonCard.jsx
+++ javascript/React/propitup/client/src/components/PersonCard.jsx
@@ -3,14 +3,25 @@ import React, { Component } from "react";
 class PersonCard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      age: this.props.age,
+    };
   }
   render() {
-    const { firstName, age, hairColor } = this.props;
+    const { firstName, lastName, hairColor } = this.props;
     return (
       <div>
-        <h2>{firstName}</h2>
-        <p>Age: {age}</p>
+        <h2>
+          {firstName}, {lastName}
+        </h2>
+        <p>Age: {this.state.age}</p>
         <p>Hair Color: {hairColor}</p>
+        <button
+          className="btn btn-primary"
+          onClick={() => this.setState({ age: this.state.age + 1 })}
+        >
+          Birthday Button for {firstName} {lastName}
+        </button>
       </div>
     );
   }
