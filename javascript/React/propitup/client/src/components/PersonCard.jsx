1e49b23adc86a04b52520506f2dd9d2ed662dea8
Harold Barleta <hbf20c@gmail.com>
2023-01-14 02:27:54 -0800

    functional component reformat format

--- javascript/React/propitup/client/src/components/PersonCard.jsx
+++ javascript/React/propitup/client/src/components/PersonCard.jsx
@@ -1,30 +1,31 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class PersonCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      age: this.props.age,
-    };
-  }
-  render() {
-    const { firstName, lastName, hairColor } = this.props;
-    return (
-      <div>
-        <h2>
-          {firstName}, {lastName}
-        </h2>
-        <p>Age: {this.state.age}</p>
-        <p>Hair Color: {hairColor}</p>
-        <button
-          className="btn btn-primary"
-          onClick={() => this.setState({ age: this.state.age + 1 })}
-        >
-          Birthday Button for {firstName} {lastName}
-        </button>
-      </div>
-    );
-  }
-}
+// class PersonCard extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       age: this.props.age,
+//     };
+//   }
+const PersonCard = (props) => {
+  const [age, setAge] = useState(props.age);
+  const { firstName, lastName, hairColor } = props;
+  const addBirthday = () => {
+    setAge(age + 1);
+  };
+
+  return (
+    <div>
+      <h2>
+        {firstName}, {lastName}
+      </h2>
+      <p>Age: {age}</p>
+      <p>Hair Color: {hairColor}</p>
+      <button className="btn btn-primary" onClick={addBirthday}>
+        Birthday Button for {firstName} {lastName}
+      </button>
+    </div>
+  );
+};
 
 export default PersonCard;
