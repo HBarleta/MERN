585850c2ae9bacf7d7cdfd8fad36e5af2663c4ee
Harold Barleta <hbf20c@gmail.com>
2023-01-18 23:33:49 -0800

    updated with axios method

--- javascript/React/pokemonapi/client/src/components/Pokemon.jsx
+++ javascript/React/pokemonapi/client/src/components/Pokemon.jsx
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Pokemon = () => {
   const [pokeContainer, setPokeContainer] = useState([]);
   const grabPokeNames = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+    // axios method
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
       .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-      .then((response) => {
-        console.log("the results are :", response.results);
-        return setPokeContainer([...response.results]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        console.log("Here is the data you requested ", response.data.results);
+        return setPokeContainer([...response.data.results]);
+      }, []);
+
+    // FETCH METHOD
+    // fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+    //   .then((response) => {
+    //     console.log(response);
+    //     return response.json();
+    //   })
+    //   .then((response) => {
+    //     console.log("the results are :", response.results);
+    //     return setPokeContainer([...response.results]);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
   };
   return (
     <div>
@@ -24,7 +34,7 @@ const Pokemon = () => {
         <ul>
           {pokeContainer.map((onePoke, idx) => {
             return (
-              <li id={idx} className="text-capitalize">
+              <li key={idx} className="text-capitalize">
                 {idx + 1 + ". "}
                 {onePoke.name}
               </li>
