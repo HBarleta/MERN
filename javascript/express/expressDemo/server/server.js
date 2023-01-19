a32c7da460ceb2d1da39ee97787b6274d0178df0
Harold Barleta <hbf20c@gmail.com>
2023-01-19 09:26:07 -0800

    added express demo

---
+++ javascript/express/expressDemo/server/server.js
@@ -0,0 +1,9 @@
+//import express and store it in a variable
+const express = require("express");
+
+// initialize the express app and store it in a variable
+
+const app = express();
+
+const port = 8000;
+
