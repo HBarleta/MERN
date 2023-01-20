f3cb23fbc67b82fb3c6ca3e68b81b005f4b9d600
Harold Barleta <hbf20c@gmail.com>
2023-01-20 05:40:26 -0800

    added routes and controller logic

--- javascript/express/expressDemo2/server/controllers/song.controller.js
+++ javascript/express/expressDemo2/server/controllers/song.controller.js
@@ -3,3 +3,24 @@ const Song = require('../models/song.model')
 module.exports.testApi = (req, res) => {
     res.json({Status: "I made capt."})
 }
+
+// Create one
+
+module.exports.createSong = (req, res) => {
+    //req.body is all the inbound data
+    const newSong = req.body
+    // Song refers to database, name was created in model file
+    Song.create(newSong)
+    // takes res and turns it into json to pass into client side
+    .then(song => res.json(song))
+    //error catch
+    .catch(err => res.json(err))
+}
+
+// Read all
+
+// Read one
+
+// Update one
+
+// Delete one
--- javascript/express/expressDemo2/server/routes/song.routes.js
+++ javascript/express/expressDemo2/server/routes/song.routes.js
@@ -3,4 +3,5 @@ const SongController = require('../controllers/song.controller');
 module.exports = (app) => {
     // all routes start with "/api"
     app.get("/api/test", SongController.testApi)
+    app.post("/api/songs", SongController.createSong)
 }
