e18551d20f079abb8bb2afb70d1f9a6e78d332cb
Harold Barleta <hbf20c@gmail.com>
2023-01-27 07:48:23 -0800

    removed unused components

--- javascript/fullstackMERN/Authors/client/src/components/AddAuthor.jsx
+++ javascript/fullstackMERN/Authors/client/src/components/AddAuthor.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import React from "react";
+import { Link } from "react-router-dom";
+
 import Form from "./Form";
 const AddAuthor = () => {
   return (
